bd3314d599c7613109c6019a365286dec2e95750
Anita Kemp and Jacqueline Remmel <anita@>
2019-09-04 11:23:50 -0700

    Add second test in prime-spec.js to test siftPrimes function.

--- spec/prime-spec.js
+++ spec/prime-spec.js
@@ -1,12 +1,18 @@
+import { siftPrimes } from '../src/scripts.js';
 
 describe('siftPrimes()', function() {
   var userInput;
 
   beforeEach(function() {
-    userInput = -3;
+    userInput = 9;
   });
 
   it ('should do nothing for any input other than a number greater than 1', function() {
     expect(userInput).toBeGreaterThan(1);
+  });
+
+  it('should list all prime numbers from 2 to user input', function(){
+    var exampleOutput = [2, 3, 5, 7];
+    expect(siftPrimes(userInput)).toEqual(exampleOutput);
   });
 });
